6043fe9e599819c1dbb9c8775e48f03c15b858cf
NaveenKumar Namachivayam <catch.nkn@gmail.com>
2020-05-06 15:02:06 -0400

    Added README

--- run.sh
+++ run.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+# Printing present working directory
 echo "Current Working Directory is: " $PWD
+
+# Printing the arguments passed
 echo "List of arguements passed: " $@
+
+# Executing the DevWeb script
 ./DevWeb $@
+
+# Printing the completion message
 echo "Execution has been completed."
